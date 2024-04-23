cef668fc20f8ad3655b370cee3c87a4a9cb85431
Yogesh Rai <yograi343@gmail.com>
2024-04-23 16:20:03 +0530

    1517 find users valid email using regex

---
+++ 1517_find_users_with_valid_email.py
@@ -0,0 +1,72 @@
+"""
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
+| mail          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains information of the users signed up in a website. Some e-mails are invalid.
+ 
+
+Write a solution to find the users who have valid emails.
+
+A valid e-mail has a prefix name and a domain where:
+
+The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+The domain is '@leetcode.com'.
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-----------+-------------------------+
+| user_id | name      | mail                    |
++---------+-----------+-------------------------+
+| 1       | Winston   | winston@leetcode.com    |
+| 2       | Jonathan  | jonathanisgreat         |
+| 3       | Annabelle | bella-@leetcode.com     |
+| 4       | Sally     | sally.come@leetcode.com |
+| 5       | Marwan    | quarz#2020@leetcode.com |
+| 6       | David     | david69@gmail.com       |
+| 7       | Shapiro   | .shapo@leetcode.com     |
++---------+-----------+-------------------------+
+Output: 
++---------+-----------+-------------------------+
+| user_id | name      | mail                    |
++---------+-----------+-------------------------+
+| 1       | Winston   | winston@leetcode.com    |
+| 3       | Annabelle | bella-@leetcode.com     |
+| 4       | Sally     | sally.come@leetcode.com |
++---------+-----------+-------------------------+
+Explanation: 
+The mail of user 2 does not have a domain.
+The mail of user 5 has the # sign which is not allowed.
+The mail of user 6 does not have the leetcode domain.
+The mail of user 7 starts with a period.
+"""
+
+import pandas as pd
+
+users = pd.DataFrame(
+    {
+        'user_id':[1,2,3,4,5,6,7],
+        'name':['Winston','Jonathan','Annabelle','Sally','Marwan','David','Shapiro'],
+        'mail':['winston@leetcode.com','jonathanisgreat','bella-@leetcode.com','sally.com@leetcode.com','quarz#2020@leetcode.com','david69@gmail.com','.shapo@leetcode.com']
+    }
+)
+
+
+def valid_emails(users:pd.DataFrame)->pd.DataFrame:
+    pattern=r'^[a-zA-Z][a-zA-Z0-9-_\.]*+@leetcode+\.com$'
+    users=users[users['mail'].str.contains(pattern)]
+    return users
+print(valid_emails(users))
